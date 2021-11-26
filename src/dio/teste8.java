f14150f7d0fde80170ef538e7651afaec59e265f
Lucas Sampaio <sampaiolucas58@gmail.com>
2021-11-26 01:14:54 -0300

    criando class livros

--- src/dio/teste8.java
+++ src/dio/teste8.java
@@ -10,6 +10,44 @@ public class teste8 {
         Gato.setCor("preto");
         Gato.setIdade(21);
         System.out.println("Hello word " + a + " " + Gato);
-        System.out.println("Hello word " + a + " " + Gato);
+        livros livro = new livros();
+        System.out.println(livro);
+    }
+    static class livros{
+        public livros(String nome, Integer numPagians) {
+            this.nome = nome;
+            this.numPagians = numPagians;
+        }
+
+        private String nome;
+        private Integer numPagians;
+
+        public livros() {
+
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Integer getNumPagians() {
+            return numPagians;
+        }
+
+        public void setNumPagians(Integer numPagians) {
+            this.numPagians = numPagians;
+        }
+
+        @Override
+        public String toString() {
+            return "livros{" +
+                    "nome='" + nome + '\'' +
+                    ", numPagians=" + numPagians +
+                    '}';
+        }
     }
 }
